98042c41e713eb3a071fd00b750353f1aed1fc1c
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-09-28 10:54:09 +0200

    Fix

--- index.php
+++ index.php
@@ -2,20 +2,18 @@
 
     require '_assets/utils/autoloader.php';
 
-    if(filter_input(INPUT_GET, 'action')){
-        if($_GET['action'] === 'inscription'){
+    if(filter_input(INPUT_GET, 'action')) {
+        if ($_GET['action'] === 'inscription') {
             (new \ChirpChat\Controllers\Inscription())->execute();
-        }
-        else if($_GET['action'] === 'connexion'){
+        } else if ($_GET['action'] === 'connexion') {
             (new \ChirpChat\Controllers\Login())->execute();
-        }
-        else if($_GET['action'] === 'recuperation'){
+        } else if ($_GET['action'] === 'recuperation') {
             (new \ChirpChat\Controllers\Recovery())->execute();
-        }
-        else if($_GET['action'] === 'registerUser'){
+        } else if ($_GET['action'] === 'registerUser') {
             (new \ChirpChat\Controllers\User)->registerUser();
-        }else{
-        (new \ChirpChat\Controllers\Homepage)->execute();
+        } else {
+            (new \ChirpChat\Controllers\Homepage)->execute();
+        }
     }
 
 ?>

32d9438e5260fab58e6417ab062e53bf17bbab2c
d22030951 <lucas.DA-SILVA-SANTOS@etu.univ-amu.fr>
2023-10-02 10:31:45 +0200

    Conditions inscriptions

--- modules/chirpChat/model/user.php
+++ modules/chirpChat/model/user.php
@@ -15,17 +15,55 @@ class User{
     }
 
     public function register($username, $pseudonyme, $email, $password, $birthdate) {
+        //a vérifier
+        if (strlen($password) < 8) {
+            throw new \Exception("Le mot de passe doit contenir au moins 8 caractères.");
+        }
+
+        $containsUppercase = false;
+        $containsLowercase = false;
+        $containsSpecialCharacter = false;
+        $containsNumber = false;
+
+        for ($i = 0; $i < strlen($password); ++$i) {
+            if (ctype_upper($password[$i])) {
+                $containsUppercase = true;
+            } elseif (ctype_lower($password[$i])) {
+                $containsLowercase = true;
+            } elseif ($password[$i] == '~' || $password[$i] == '@' || $password[$i] == '_' || $password[$i] == '/' || $password[$i] == '+' ||
+                $password[$i] == ':' || $password[$i] == '*' || $password[$i] == '!') {
+                $containsSpecialCharacter = true;
+            } elseif (ctype_digit($password[$i])) {
+                $containsNumber = true;
+            }
+
+            if ($containsUppercase && $containsLowercase && $containsSpecialCharacter && $containsNumber) {
+                break;
+            }
+        }
+
+        if (!$containsUppercase || !$containsLowercase) {
+            throw new \Exception("Le mot de passe doit contenir au moins une lettre majuscule et une lettre minuscule.");
+        }
+        if (!$containsSpecialCharacter) {
+            throw new \Exception("Le mot de passe doit contenir au moins un caractère spécial parmi : \"~ , @ , _ , / , + , : , * , !\".");
+        }
+        if (!$containsNumber) {
+            throw new \Exception("Le mot de passe doit contenir au moins un chiffre.");
+        }
+        //...
         if(!$statement = $this->connection->getConnection()->query("INSERT INTO Utilisateur (id_utilisateur, username, pseudonyme, email, mot_passe, date_naissance) 
         VALUES (uuid(), '$username', '$pseudonyme', '$email', '$password', '$birthdate')")){
             throw new \PDOException('erreur sql');
         }
     }
-
-
-
-
-
 }
+
+
+
+
+
+
 
 
 

1a75fff931b85446851d715582bba272999d2d1e
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-10-02 11:07:58 +0200

    Début page de récuperation

--- modules/chirpchat/views/auth/recovery.php
+++ modules/chirpchat/views/auth/recovery.php
@@ -8,6 +8,17 @@ class Recovery {
         ob_start();
         ?>
 
+    <form id="loginForm" action="index.php?action=loginUser" method="post">
+        <p>
+            <br>
+            <input id="recoveryChamps" type="text" name="email" placeholder="E-mail de récuperation"> <br> <!-- L'utilisateur rentre son e-mail ici -->
+
+            <br>
+            <input id="recoveryChamps" type="text" name="placeholder" placeholder="placeholder"> <br>
+
+            <br>
+            <input id="submitRecovery" type="submit" value="Réinitialiser mot de passe"><br> <!-- Bouton pour valider les champs -->
+
         <?php
         $content = ob_get_clean();
         (new \ChirpChat\Views\MainLayout("Mot de passe oublié", $content))->show();

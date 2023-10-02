8b10002d1231018ed5b5022105077994e8ec44a3
b22002597 <rayan.bendjeddou@etu.univ-amu.fr>
2023-10-02 08:57:44 +0200

    Identifiants base de données cachés

--- modules/chirpchat/model/database.php
+++ modules/chirpchat/model/database.php
@@ -1,19 +1,15 @@
 <?php
 
 namespace Chirpchat\Model;
-
+require_once 'dbCredentials.php';
 class Database {
 
     private static $_instance = null;
-    private string $host = 'mysql-devchirpchat.alwaysdata.net';
-    private string $username = '327978_raph';
-    private string $password = 'Raphael123!';
-    private string $dbName = 'devchirpchat_db';
     private \PDO $connection;
 
     public function __construct()
     {
-        $this->connection = new \PDO('mysql:host=mysql-devchirpchat.alwaysdata.net;dbname=devchirpchat_db;charset=utf8', $this->username, $this->password)  or die(mysqli_error($this->connection));
+        $this->connection = new \PDO('mysql:host='.HOST.';dbname='.DBNAME, USERNAME, PASSWORD)  or die(mysqli_error($this->connection));
     }
 
     public static function getInstance(): Database
@@ -24,7 +20,7 @@ class Database {
     }
 
     public function connect() : void {
-        $this->connection = new \PDO($this->host, $this->username, $this->password, $this->dbName) or die(mysqli_error($this->connection));
+        $this->connection = new \PDO(HOST, USERNAME, PASSWORD, DBNAME) or die(mysqli_error($this->connection));
     }
 
     public function getConnection(){

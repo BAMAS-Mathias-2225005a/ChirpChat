a108139609781490edcb2fdeb59f8e5e01d13d05
b22002597 <rayan.bendjeddou@etu.univ-amu.fr>
2023-10-06 11:03:14 +0200

    fix credentials bd

--- modules/chirpchat/model/database.php
+++ modules/chirpchat/model/database.php
@@ -6,15 +6,11 @@ class Database {
 
     private static $_instance = null;
     private $dbAccess;
-    private string $host = 'mysql-devchirpchat.alwaysdata.net';
-    private string $username = '327978_raph';
-    private string $password = 'Raphael123!';
-    private string $dbName = 'devchirpchat_db';
     private \PDO $connection;
 
     public function __construct()
     {
-        $this->dbAccess = parse_ini_file('_assets\cred\db.ini');
+        $this->dbAccess = parse_ini_file(realpath('/home/devchirpchat/www/_assets/cred/db.ini'));
         $this->connection = new \PDO($this->dbAccess['dsn'], $this->dbAccess['username'], $this->dbAccess['password'])  or die(mysqli_error($this->connection));
     }
 

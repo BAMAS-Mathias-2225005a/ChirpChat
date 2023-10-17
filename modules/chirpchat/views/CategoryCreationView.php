b0384bbb46d41d170df5907b28317e494ded0998
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2023-10-17 14:51:23 +0200

    CSS page création catégorie

--- modules/chirpchat/views/CategoryCreationView.php
+++ modules/chirpchat/views/CategoryCreationView.php
@@ -7,15 +7,17 @@ class CategoryCreationView {
     public function displayCategoryCreationForm() : void {
         ob_start();
         ?>
-        <h2>CREATION DE CATÉGORIE</h2>
-        <form method="post" action="index.php?action=createCategory">
-            <input type="text" placeholder="Nom de la catégorie" name="categoryName" required>
-            <textarea placeholder="Description de la catégorie" name="categoryDescription" required></textarea>
-            <input type="submit">
+        <h2 id="titreCreation">Création catégorie</h2>
+        <form id="zoneCreation" method="post" action="index.php?action=createCategory">
+            <input id="nomCategorie" type="text" placeholder="Nom de la catégorie" name="categoryName" required><br/>
+            <br/>
+            <textarea id="descriptionCategorie" placeholder="Description de la catégorie" name="categoryDescription" required></textarea><br/>
+            <br/>
+            <input id="envoiCreation" type="submit">
         </form>
         <?php
         $pageContent = ob_get_clean();
-        (new \ChirpChat\Views\MainLayout("Creation de catégorie", $pageContent))->show(['categoryCreation.css']);
+        (new \ChirpChat\Views\MainLayout("Creation catégorie", $pageContent))->show(['categoryCreation.css']);
     }
 
 }

d237528ca6bcd40e787176a6d3623c21b358cb7d
ByOnyx <byonyxmc@gmail.com>
2023-10-23 01:48:32 +0200

    Suppression de la possibilité d'ouvrir plusieurs menus en meme temps

--- _assets/js/navbar.js
+++ _assets/js/navbar.js
@@ -1,9 +1,18 @@
 function openCloseUserMenu(menu){
+    closeAllOtherMenu()
+
     var menuDeroulant = menu.children[1];
     if(menuDeroulant.className === 'menuOpen'){
         menuDeroulant.className = 'menuClose';
     }else{
         menuDeroulant.className = 'menuOpen'
+    }
+}
+
+function closeAllOtherMenu(){
+    const allMenu = document.getElementsByClassName("menuOpen");
+    for(const m of allMenu){
+        m.className = 'menuClose';
     }
 }
 

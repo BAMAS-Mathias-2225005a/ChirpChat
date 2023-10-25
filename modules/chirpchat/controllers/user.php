bdfaa065d2132f78f33963c716cefe079ab9a277
d22030951 <lucas.da-silva-santos@etu.univ-amu.fr>
2023-10-25 10:55:09 +0200

    ajout verif mdp

--- modules/chirpchat/controllers/user.php
+++ modules/chirpchat/controllers/user.php
@@ -31,6 +31,7 @@ class User {
     }
 
     public function register() : void{
+        if(!$this->isRegisterValid($_POST['username'],$_POST['pseudonyme'], $_POST['email'], $_POST['password'], $_POST['birthdate'])) return;
         (new \ChirpChat\Model\UserRepository(Database::getInstance()->getConnection()))->register($_POST['username'],$_POST['pseudonyme'],$_POST['email'],$_POST['password'],$_POST['birthdate']);
         header("Location:index.php");
     }
@@ -48,5 +49,47 @@ class User {
     public function logout() : void{
         session_destroy();
         header("Location:index.php");
+    }
+
+    public function isRegisterValid($username, $pseudonyme, $email, $password, $birthdate)
+    {
+        //a vérifier
+        if (strlen($password) < 8) {
+            throw new \Exception("Le mot de passe doit contenir au moins 8 caractères.");
+        }
+
+        $containsUppercase = false;
+        $containsLowercase = false;
+        $containsSpecialCharacter = false;
+        $containsNumber = false;
+
+        for ($i = 0; $i < strlen($password); ++$i) {
+            if (ctype_upper($password[$i])) {
+                $containsUppercase = true;
+            } elseif (ctype_lower($password[$i])) {
+                $containsLowercase = true;
+            } elseif ($password[$i] == '~' || $password[$i] == '@' || $password[$i] == '_' || $password[$i] == '/' || $password[$i] == '+' ||
+                $password[$i] == ':' || $password[$i] == '*' || $password[$i] == '!') {
+                $containsSpecialCharacter = true;
+            } elseif (ctype_digit($password[$i])) {
+                $containsNumber = true;
+            }
+
+            if ($containsUppercase && $containsLowercase && $containsSpecialCharacter && $containsNumber) {
+                break;
+            }
+        }
+
+        if (!$containsUppercase || !$containsLowercase) {
+            throw new \Exception("Le mot de passe doit contenir au moins une lettre majuscule et une lettre minuscule.");
+        }
+        if (!$containsSpecialCharacter) {
+            throw new \Exception("Le mot de passe doit contenir au moins un caractère spécial parmi : \"~ , @ , _ , / , + , : , * , !\".");
+        }
+        if (!$containsNumber) {
+            throw new \Exception("Le mot de passe doit contenir au moins un chiffre.");
+        }
+        //...
+        return true;
     }
 }
